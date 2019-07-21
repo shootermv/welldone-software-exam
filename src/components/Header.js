6d4534deca8e27d7a3cbe1eab5e165898dd09b66
moshe <moshe.vilner@webintpro.com>
2019-07-21 17:36:52 +0300

    header menus should stick to the right

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+
+import ListIcon from '@material-ui/icons/List';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+
 const useStyles = makeStyles(theme => ({
     root: {
       flexGrow: 1,
@@ -13,19 +23,51 @@ const useStyles = makeStyles(theme => ({
     title: {
       flexGrow: 1,
     },
+    rightToolbar: {
+        marginLeft: 'auto',
+        marginRight: -12,
+    }
   }));
 function Header({title}) {
     const classes = useStyles();
-  
+    const [value, setValue] = React.useState(0);
+
+    function handleChange(event, newValue) {
+        setValue(newValue);
+    }
     return (
-            <AppBar position="static">
-               
-               <Toolbar>
-    
-                <Typography variant="h6" className={classes.title}>
+            <AppBar position="static">  
+
+              <Grid
+                justify="space-between" // Add it here :)
+                container 
+                spacing={24}
+                >
+                <Grid item>
+                  <Toolbar>
+                    <Typography type="title" color="inherit">
                     {title}
-                </Typography>
-                </Toolbar>
+                    </Typography>
+                  </Toolbar>
+                </Grid>
+
+                <Grid item>
+                    <Tabs
+                        className={classes.rightToolbar}
+                        value={value}
+                        onChange={handleChange}
+                        variant="scrollable"
+                        scrollButtons="off"
+                        aria-label="Scrollable prevent tabs example"
+                        >
+                        <Tab icon={<ListIcon />} aria-label="Phone"  />
+                        <Tab icon={<EditIcon />} aria-label="Favorite"  />
+                        <Tab icon={<DeleteIcon />} aria-label="Person"  />
+                        <Tab icon={<AddCircleIcon />} aria-label="Help" />
+
+                    </Tabs> 
+                </Grid>
+               </Grid>           
             </AppBar>
         )
     }
