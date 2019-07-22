a782183676dfe3e37567d799b25951cd31056069
shootermv <shootermv@gmail.com>
2019-07-23 01:13:11 +0300

    removed unneeded code

--- src/index.js
+++ src/index.js
@@ -6,22 +6,6 @@ import App from './App';
 import theme from './theme';
 
 
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-// stupid hack so that leaflet's images work after going through webpack
-import marker from 'leaflet/dist/images/marker-icon.png';
-import marker2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: marker2x,
-    iconUrl: marker,
-    shadowUrl: markerShadow
-});
-
-
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
